f057f5dab11b3b714b361012b2fe5412306707d2
trossides <theorossides@gmail.com>
2018-09-28 15:44:29 +0930

    week 10 updates

--- theo.js
+++ theo.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 class Theo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+
+      textValue :''
+    };
   }
-    
+
     render() {
+        const length = this.state.textValue.length;
+
         return (<View>
-        <Text>Theo Test!</Text>
-        <Button onPress={this.props.onPress} title="Press Me"/>
+
+        <Text>Theo App Example!</Text>
+        <Text>Number of characters: {length}</Text>
+        <Button onPress={this.props.onPress} title="Press Me - Toggle"/>
+        
+        <TextInput
+        style={{height: 80}}
+        onChangeText={(text) => this.setState({textValue : text})}
+        multiline={true} />
+        
+        <Button onPress={() => {Alert.alert('You are awesome!');}} title="Press Me :)"/>
+
+
         </View>);
+
+        
   }
+  
 }
 
 export default Theo;
